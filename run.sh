0851fdaddc6ca234655b218c0b11936c9d4b5896
Christopher Harrison <ch12@sanger.ac.uk>
2017-01-11 16:28:29 +0000

    Generate irods_environment.json from template, rather than bind mounting

--- run.sh
+++ run.sh
@@ -5,6 +5,5 @@ set -eu -o pipefail
 USER="${1:-$(whoami)}"
 
 docker run -v /etc/krb5.conf:/etc/krb5.conf \
-           -v $(pwd)/irods_environment.json:/home/${USER}/.irods/irods_environment.json \
            -it "irods-kerberos-test:${USER}" \
            /bin/bash
