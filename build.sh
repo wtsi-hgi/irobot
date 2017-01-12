5715ffeb054dcd893e3f833e6d20b8f01057ea9f
Christopher Harrison <ch12@sanger.ac.uk>
2017-01-12 10:32:16 +0000

    Add copyright/license boilerplate

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Copyright (c) 2017 Genome Research Ltd.
+# 
+# Author: Christopher Harrison <ch12@sanger.ac.uk>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 set -eu -o pipefail
 
 get_krb_realm() {
@@ -83,7 +100,7 @@ main() {
 
   local dockerfile="$(create_dockerfile "${user}" "${password}" "${krb_realm}")"
 
-  docker build -t "irods-kerberos-test:${user}" -f "${dockerfile}" .
+  docker build -t "hgi/irobot:${user}" -f "${dockerfile}" .
 }
 
 main "${1:-$(whoami)}"
