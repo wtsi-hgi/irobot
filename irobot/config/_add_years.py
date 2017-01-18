cfbda87f0df1ab844f820f014802b25eaa0f68c5
Christopher Harrison <ch12@sanger.ac.uk>
2017-01-18 16:57:01 +0000

    Fractional year arithmetic

---
+++ irobot/config/_add_years.py
@@ -0,0 +1,64 @@
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Author: Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import math
+from datetime import datetime, timedelta
+from fractions import Fraction
+from types import IntType, FloatType
+
+
+def _multiply_timedelta(delta, m):
+    """
+    Multiply a timedelta by m, where m can be decimal
+
+    @param   delta  Input timedelta (timedelta)
+    @param   m      Multiplier (numeric)
+    @return  Multiplied timedelta (timedelta)
+    """
+    assert isinstance(delta, timedelta), "Expecting timedelta, not %s" % type(timedelta)
+    assert type(m) in [IntType, FloatType], "Expecting numeric multiplier, not %s" % type(m)
+
+    frac_m = Fraction(m).limit_denominator(1000)
+    return (delta * frac_m.numerator) / frac_m.denominator
+
+
+def add_years(timestamp, years):
+    """
+    Add a number of years (integer or decimal, positive or negative) to
+    a given timestamp
+
+    @param   timestamp  Input timestamp in UTC (datetime)
+    @param   years      Years to add (numeric)
+    @return  Shifted timestamp in UTC (datetime)
+    """
+    assert isinstance(timestamp, datetime), "Expecting datetime, not %s" % type(timestamp)
+    assert type(years) in [IntType, FloatType], "Expecting numeric shift, not %s" % type(years)
+
+    if years == 0:
+        return timestamp
+
+    whole_shift = timestamp.replace(year = timestamp.year + int(years))
+
+    over_years = int(math.ceil(years) if cmp(years, 0) == 1 else math.floor(years))
+    over_shift = timestamp.replace(year = timestamp.year + over_years)
+
+    frac_years = abs(years - int(years))
+    frac_delta = _multiply_timedelta(over_shift - whole_shift, frac_years)
+
+    return whole_shift + frac_delta
