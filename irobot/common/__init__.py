ec76afe6e23a09a130f9cd9a6523312919503c91
Christopher Harrison <ch12@sanger.ac.uk>
2017-01-27 11:04:59 +0000

    Type checking for homogeneous collections

--- irobot/common/__init__.py
+++ irobot/common/__init__.py
@@ -1,3 +1,3 @@
-from irobot.common.type_check import type_check
+from irobot.common.type_check import type_check, type_check_collection
 from irobot.common.datetime_arithmetic import multiply_timedelta, add_years
 from irobot.common.canonical_path import canonical_path
