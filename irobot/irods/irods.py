fd3da721584834ba58dcd729458f70b80141486b
Christopher Harrison <ch12@sanger.ac.uk>
2017-02-03 14:28:07 +0000

    Resource managed iRODS interface with tests

--- irobot/irods/irods.py
+++ irobot/irods/irods.py
@@ -17,14 +17,40 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from types import StringType
+from collections import deque
+from datetime import datetime
+from inspect import getargspec, ismethod
+from subprocess import CalledProcessError
+from threading import BoundedSemaphore, Thread
+from types import FunctionType, MethodType, StringType
 
 from irobot.common import type_check
 from irobot.config.irods import iRODSConfig
+from irobot.irods._api import baton, iget, ils
+
+
+# Status constants
+IGET_QUEUED = "queued"
+IGET_STARTED = "started"
+IGET_FINISHED = "finished"
+IGET_FAILED = "failed"
+
+
+def _exists(irods_path):
+    """
+    Check data object exists on iRODS
+
+    @param   irods_path  Path to data object on iRODS (string)
+    """
+    try:
+        ils(irods_path)
+
+    except CalledProcessError:
+        raise IOError("Data object \"%s\" inaccessible" % irods_path)
 
 
 class iRODS(object):
-    """ High level iRODS interface """
+    """ High level iRODS interface with iget pool management """
     def __init__(self, irods_config):
         """
         Constructor
@@ -32,6 +58,58 @@ class iRODS(object):
         @param   irods_config  iRODS configuration
         """
         type_check(irods_config, iRODSConfig)
+        self._config = irods_config
+
+        self._iget_queue = deque()  # n.b., collections.deque is thread-safe
+        self._iget_pool = BoundedSemaphore(self._config.max_connections())
+
+        self._listeners = []
+
+        self._running = True
+        self._runner = Thread(target=self._thread_runner)
+        self._runner.daemon = True
+        self._runner.start()
+
+    def _thread_runner(self):
+        """ Thread runner to invoke igets """
+        while self._running:
+            if self._iget_queue:
+                iget_args = self._iget_queue.popleft()
+                with self._iget_pool:
+                    Thread(target=self._iget, args=iget_args).start()
+
+    def __del__(self):
+        """ Stop thread runner on GC """
+        self._running = False
+
+    def add_listener(self, listener):
+        """
+        Add a listener for broadcast messages
+
+        @param   listener  Listener (function of three arguments)
+        """
+        type_check(listener, FunctionType, MethodType)
+
+        # Methods include the "self" argument
+        arg_len = 4 if ismethod(listener) else 3
+        if __debug__ and len(getargspec(listener).args) != arg_len:
+            raise TypeError("Listener doesn't take 3 arguments")
+
+        self._listeners.append(listener)
+
+    def _broadcast(self, status, irods_path):
+        """
+        Broadcast a message to all the listeners
+
+        @param   status      Message status (string)
+        @param   irods_path  Path to data object on iRODS (string)
+        """
+        type_check(status, StringType)
+        type_check(irods_path, StringType)
+
+        broadcast_time = datetime.utcnow()
+        for listener in self._listeners:
+            listener(broadcast_time, status, irods_path)
 
     def get_dataobject(self, irods_path, local_path):
         """
@@ -44,12 +122,39 @@ class iRODS(object):
         type_check(irods_path, StringType)
         type_check(local_path, StringType)
 
-    def get_metadata(self, irods_path):
+        _exists(irods_path)
+        self._iget_queue.append((irods_path, local_path))
+        self._broadcast(IGET_QUEUED, irods_path)
+
+    def _iget(self, irods_path, local_path):
+        """
+        Perform the iget and broadcast status
+
+        @note    Blocking
+
+        @param   irods_path  Path to data object on iRODS (string)
+        @param   local_path  Local filesystem target file (string)
+        """
+        try:
+            self._broadcast(IGET_STARTED, irods_path)
+            iget(irods_path, local_path)
+            self._broadcast(IGET_FINISHED, irods_path)
+
+        except CalledProcessError:
+            self._broadcast(IGET_FAILED, irods_path)
+
+    @staticmethod
+    def get_metadata(irods_path):
         """
         Retrieve AVU and filesystem metadata for data object from iRODS
 
         @param   irods_path  Path to data object on iRODS (string)
-        @return  AVU and filesystem metadata (tuple of dictionaries)
-        @note    AVUs will have their units stripped off
+        @return  AVU and filesystem metadata (tuple of list and dictionary)
         """
         type_check(irods_path, StringType)
+
+        _exists(irods_path)
+        baton_output = baton(irods_path)
+
+        fs_keys = ["checksum", "size", "access", "timestamps"]
+        return baton_output["avus"], {k:v for k, v in baton_output.items() if k in fs_keys}
--- irobot/test/irods/test_irods.py
+++ irobot/test/irods/test_irods.py
@@ -17,55 +17,235 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import os
 import unittest
-import filecmp
-from tempfile import gettempdir, NamedTemporaryFile
+from subprocess import CalledProcessError
 
+from mock import MagicMock, call
+
+import irobot.irods.irods as irods
 from irobot.config.irods import iRODSConfig
-from irobot.irods import iRODS
 
 
 # Temporary data object for testing
-TEMP_DATAOBJECT_SIZE = 1024
-TEMP_DATAOBJECT_DATA = os.urandom(TEMP_DATAOBJECT_SIZE)
-TEMP_DATAOBJECT_METADATA = {"foo": "bar"}
+TEST_DATAOBJECT_METADATA = {
+  "collection": "/foo",
+  "data_object": "bar",
+  "checksum": "abcdef1234567890",
+  "size": 1234,
+  "avus": [
+    {"attribute": "foo", "value": "bar"},
+    {"attribute": "quux", "value": "xyzzy"}
+  ],
+  "access": [
+    {"owner": "someone", "level": "own", "zone": "myZone"}
+  ],
+  "timestamps": [
+    {"created": "1970-01-01T00:00:00", "replicates": 0},
+    {"modified": "1970-01-01T00:00:00", "replicates": 0}
+  ]
+}
 
 
-# TODO Mock low-level calls to get data and metadata so they always
-# return the above
+irods.baton = MagicMock(return_value=TEST_DATAOBJECT_METADATA)
+irods.ils = MagicMock()
+irods.iget = MagicMock()
+
+
+class TestExists(unittest.TestCase):
+    def tearDown(self):
+        irods.ils.reset_mock()
+        irods.ils.side_effect = None
+
+    def test_exists_pass(self):
+        irods._exists("/foo/bar")
+        irods.ils.assert_called_once_with("/foo/bar")
+
+    def test_exists_fail(self):
+        irods.ils.side_effect = CalledProcessError(1, None, None)
+        self.assertRaises(IOError, irods._exists, "/foo/bar")
 
 
 class TestiRODS(unittest.TestCase):
     def setUp(self):
-        config = iRODSConfig(max_connections="2")
-        self.irods = iRODS(config)
-
-        self.do_source = NamedTemporaryFile(delete=False)
-        self.do_source.write(TEMP_DATAOBJECT_DATA)
-        self.do_source.close()
-
-        # We only create the target for the sake of the filename
-        self.do_target = NamedTemporaryFile(delete=True)
-        self.do_target.close()
-
+        config = iRODSConfig(max_connections="1")
+        self.irods = irods.iRODS(config)
+    
     def tearDown(self):
-        os.unlink(self.do_source.name)
+        irods.baton.reset_mock()
+        irods.ils.reset_mock()
+        irods.ils.side_effect = None
+        irods.iget.reset_mock()
+        irods.iget.side_effect = None
 
-        if os.path.exists(self.do_target.name):
-            os.unlink(self.do_target.name)
+        # Stop the thread runner
+        self.irods._running = False
+        self.irods._runner.join()
 
-    def test_get_dataobject(self):
-        self.irods.get_dataobject("/foo/bar", self.do_target.name)
-        self.assertTrue(filecmp.cmp(self.do_source.name, self.do_target.name))
+    def test_destructor(self):
+        self.irods.__del__()
+        self.assertFalse(self.irods._running)
+
+    def test_add_listener(self):
+        class _listeners(object):
+            def bad(self, a, b, c, d):
+                pass
+
+            def good(self, a, b, c):
+                pass
+
+        def _listener_bad(a, b, c, d):
+            pass
+
+        def _listener_good(a, b, c):
+            pass
+
+        _listener = _listeners()
+
+        self.assertRaises(TypeError, self.irods.add_listener, _listener_bad)
+        self.assertRaises(TypeError, self.irods.add_listener, _listener.bad)
+
+        self.assertIsNone(self.irods.add_listener(_listener_good))
+        self.assertEqual(len(self.irods._listeners), 1)
+        self.assertEqual(self.irods._listeners.pop(), _listener_good)
+
+        self.assertIsNone(self.irods.add_listener(_listener.good))
+        self.assertEqual(len(self.irods._listeners), 1)
+        self.assertEqual(self.irods._listeners.pop(), _listener.good)
+
+    def test_broadcast(self):
+        # Bypass type checking for listeners
+        _old_type_check, irods.type_check = irods.type_check, MagicMock()
+        _old_getargspec, irods.getargspec = irods.getargspec, MagicMock()
+        irods.getargspec().args = [1, 2, 3]
+
+        _old_datetime, irods.datetime = irods.datetime, MagicMock()
+        irods.datetime.utcnow = MagicMock(return_value=1234)
+
+        _listener = MagicMock()
+        self.irods.add_listener(_listener)
+
+        self.irods._broadcast("foo", "/foo/bar")
+        _listener.assert_called_once_with(1234, "foo", "/foo/bar")
+
+        # Reset everything
+        irods.type_check = _old_type_check
+        irods.getargspec = _old_getargspec
+        irods.datetime = _old_datetime
+
+    def test_enqueue_dataobject(self):
+        # Bypass type checking for listeners
+        _old_type_check, irods.type_check = irods.type_check, MagicMock()
+        _old_getargspec, irods.getargspec = irods.getargspec, MagicMock()
+        irods.getargspec().args = [1, 2, 3]
+
+        _old_datetime, irods.datetime = irods.datetime, MagicMock()
+        irods.datetime.utcnow = MagicMock(return_value=1234)
+
+        # Stop the thread runner
+        self.irods._running = False
+        self.irods._runner.join()
+
+        _listener = MagicMock()
+        self.irods.add_listener(_listener)
+
+        self.irods.get_dataobject("/foo/bar", "/quux/xyzzy")
+        _listener.assert_called_once_with(1234, irods.IGET_QUEUED, "/foo/bar")
+        self.assertEqual(len(self.irods._iget_queue), 1)
+        self.assertEqual(self.irods._iget_queue.pop(), ("/foo/bar", "/quux/xyzzy"))
+
+        # Reset everything
+        irods.type_check = _old_type_check
+        irods.getargspec = _old_getargspec
+        irods.datetime = _old_datetime
+
+    def test_iget_pool(self):
+        # Stop the thread runner...
+        self.irods._running = False
+        self.irods._runner.join()
+
+        _old_thread, irods.Thread = irods.Thread, MagicMock()
+        def _stop_running(*args, **kwargs):
+            self.irods._running = False
+        irods.Thread().start = _stop_running
+
+        # ...start it up again with two items in the queue
+        self.irods._iget_queue.append(("/foo/bar", "/quux/xyzzy"))
+        self.irods._iget_queue.append(("/another/test", "/fizz/buzz"))
+        self.irods._running = True
+        self.irods._thread_runner()
+
+        irods.Thread.assert_has_calls([
+            call(args=("/foo/bar", "/quux/xyzzy"), target=self.irods._iget)
+        ])
+
+        # Our instance only allows one iget at a time (i.e.,
+        # max_connections=1), therefore -- as the runner is forcefully
+        # terminated -- there should still be an element in the queue
+        # FIXME This will be true regardless of the resource locking
+        self.assertEqual(len(self.irods._iget_queue), 1)
+        self.assertEqual(self.irods._iget_queue.pop(), ("/another/test", "/fizz/buzz"))
+
+        irods.Thread = _old_thread
+
+    def test_iget_pass(self):
+        # Bypass type checking for listeners
+        _old_type_check, irods.type_check = irods.type_check, MagicMock()
+        _old_getargspec, irods.getargspec = irods.getargspec, MagicMock()
+        irods.getargspec().args = [1, 2, 3]
+
+        _old_datetime, irods.datetime = irods.datetime, MagicMock()
+        irods.datetime.utcnow = MagicMock(return_value=1234)
+
+        _listener = MagicMock()
+        self.irods.add_listener(_listener)
+
+        self.irods._iget("/foo/bar", "/quux/xyzzy")
+        irods.iget.assert_called_once_with("/foo/bar", "/quux/xyzzy")
+        _listener.assert_has_calls([
+            call(1234, irods.IGET_STARTED, "/foo/bar"),
+            call(1234, irods.IGET_FINISHED, "/foo/bar")
+        ])
+
+        # Reset everything
+        irods.type_check = _old_type_check
+        irods.getargspec = _old_getargspec
+        irods.datetime = _old_datetime
+
+    def test_iget_fail(self):
+        # Bypass type checking for listeners
+        _old_type_check, irods.type_check = irods.type_check, MagicMock()
+        _old_getargspec, irods.getargspec = irods.getargspec, MagicMock()
+        irods.getargspec().args = [1, 2, 3]
+
+        _old_datetime, irods.datetime = irods.datetime, MagicMock()
+        irods.datetime.utcnow = MagicMock(return_value=1234)
+
+        _listener = MagicMock()
+        self.irods.add_listener(_listener)
+
+        irods.iget.side_effect = CalledProcessError(1, "foo", "bar")
+
+        self.irods._iget("/foo/bar", "/quux/xyzzy")
+        irods.iget.assert_called_once_with("/foo/bar", "/quux/xyzzy")
+        _listener.assert_has_calls([
+            call(1234, irods.IGET_STARTED, "/foo/bar"),
+            call(1234, irods.IGET_FAILED, "/foo/bar")
+        ])
+
+        # Reset everything
+        irods.type_check = _old_type_check
+        irods.getargspec = _old_getargspec
+        irods.datetime = _old_datetime
 
     def test_get_metadata(self):
         avu_metadata, fs_metadata = self.irods.get_metadata("/foo/bar")
-        self.assertEqual(avu_metadata, TEMP_DATAOBJECT_METADATA)
-        self.assertEqual(fs_metadata, {"size": TEMP_DATAOBJECT_SIZE})
+        irods.ils.assert_called_once_with("/foo/bar")
+        irods.baton.assert_called_once_with("/foo/bar")
 
-    def test_max_connections(self):
-        pass
+        self.assertEqual(avu_metadata, TEST_DATAOBJECT_METADATA["avus"])
+
+        for k in ["checksum", "size", "access", "timestamps"]:
+            self.assertEqual(fs_metadata[k], TEST_DATAOBJECT_METADATA[k])
 
 
 if __name__ == "__main__":
