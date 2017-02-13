64ed6029c12a5c8e0d0ca6d2c084cf1e87fa2fb1
Christopher Harrison <ch12@sanger.ac.uk>
2017-02-13 17:24:23 +0000

    Configuration loading for HTTP basic authentication

--- irobot/config/authentication/basic.py
+++ irobot/config/authentication/basic.py
@@ -17,10 +17,107 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
+import re
+from ConfigParser import ParsingError
+from types import IntType, FloatType, NoneType, StringType
+
+import requests
+
+from irobot.common import type_check_arguments, type_check_return
 from irobot.config._base import BaseConfig
 
 
+@type_check_return(StringType)
+@type_check_arguments(url=StringType)
+def _parse_url(url):
+    """
+    Check validation URL is accepting connections
+
+    @param   url  Validation URL (string)
+    @return  Validation URL (string)
+    """
+    try:
+        # Prepend "http://" if it doesn't exist
+        if not re.match(r"^https?://", url):
+            url = "http://" + url
+
+        # FIXME? Is this a reasonable thing to do?
+        requests.head(url, timeout=0.25)
+
+    except requests.Timeout:
+        raise ParsingError("Didn't receive a response from %s in a reasonable amount of time" % url)
+
+    except Exception:
+        raise ParsingError("Couldn't connect to %s" % url)
+
+    return url
+
+
+@type_check_return(FloatType, NoneType)
+@type_check_arguments(cache=StringType)
+def _parse_cache(cache):
+    """
+    Parse revalidation cache time := "never"
+                                   | NUMERIC TIME_UNIT
+
+    @param   cache  Cache time (string)
+    @return  Cache time in seconds (numeric); None for no caching
+    """
+    if cache.lower() == "never":
+        return None
+
+    match = re.match(r"""
+        ^(?:
+            (?P<value>
+                \d+
+                (?: \. \d+ )?
+            )
+            \s*
+            (?P<unit>
+                (?: s (ec (ond)? s?)? )  # s / sec(s) / second(s)
+                |
+                (?: m (in (ute)? s?)? )  # m / min(s) / minute(s)
+            )
+        )$
+    """, cache, re.VERBOSE | re.IGNORECASE)
+
+    if not match:
+        raise ParsingError("Invalid revalidation cache time")
+
+    multiplier = 60 if match.group("unit").startswith("m") else 1
+    value = float(match.group("value")) * multiplier
+
+    # n.b., Zero seconds is the same as "never"
+    return value or None
+
+
 class BasicAuthConfig(BaseConfig):
-    def __init__(*args, **kwargs):
-        # TODO
-        pass
+    """ HTTP basic authentication configuration """
+    @type_check_arguments(url=StringType, cache=StringType)
+    def __init__(self, url, cache):
+        """
+        Parse HTTP basic authentication configuration
+
+        @param   url    Validation URL (string)
+        @param   cache  Cache time for correct validation response (string)
+        """
+        self._url = _parse_url(url)
+        self._cache = _parse_cache(cache)
+
+    @type_check_return(StringType)
+    def url(self):
+        """
+        Get validation URL
+
+        @return  Validation URL (string)
+        """
+        return self._url
+
+    @type_check_return(FloatType, NoneType)
+    def cache(self):
+        """
+        Get revalidation time
+
+        @return  Cache time in seconds (numeric); None for no caching
+        """
+        return self._cache
---
+++ irobot/test/config/authentication/test_basic.py
@@ -0,0 +1,79 @@
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Author: Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import unittest
+from ConfigParser import ParsingError
+
+import requests
+from mock import MagicMock
+
+import irobot.config.authentication.basic as basic_conf
+
+
+class TestBasicAuthConfig(unittest.TestCase):
+    def setUp(self):
+        self._old_requests_head, basic_conf.requests.head = basic_conf.requests.head, MagicMock()
+
+    def tearDown(self):
+        basic_conf.requests.head = self._old_requests_head
+
+    def test_parse_bad_url(self):
+        parse_url = basic_conf._parse_url
+        basic_conf.requests.head = self._old_requests_head
+        self.assertRaises(ParsingError, parse_url, "foo")
+
+    def test_parse_timeout_url(self):
+        parse_url = basic_conf._parse_url
+        basic_conf.requests.head.side_effect = requests.Timeout
+        self.assertRaises(ParsingError, parse_url, "foo")
+
+    def test_parse_good_url(self):
+        parse_url = basic_conf._parse_url
+
+        self.assertEqual(parse_url("http://www.sanger.ac.uk"), "http://www.sanger.ac.uk")
+        self.assertEqual(parse_url("sanger.ac.uk"), "http://sanger.ac.uk")
+
+    def test_parse_cache(self):
+        parse_cache = basic_conf._parse_cache
+
+        self.assertRaises(ParsingError, parse_cache, "foo")
+        self.assertIsNone(parse_cache("never"))
+        self.assertIsNone(parse_cache("0s"))
+        self.assertEqual(parse_cache("1s"), 1.0)
+        self.assertEqual(parse_cache("1.2 s"), 1.2)
+        self.assertEqual(parse_cache("1 sec"), 1.0)
+        self.assertEqual(parse_cache("2 secs"), 2.0)
+        self.assertEqual(parse_cache("3 second"), 3.0)
+        self.assertEqual(parse_cache("4 seconds"), 4.0)
+        self.assertEqual(parse_cache("1m"), 60.0)
+        self.assertEqual(parse_cache("1.2 m"), 72.0)
+        self.assertEqual(parse_cache("1 min"), 60.0)
+        self.assertEqual(parse_cache("2 mins"), 120.0)
+        self.assertEqual(parse_cache("3 minute"), 180.0)
+        self.assertEqual(parse_cache("4 minutes"), 240.0)
+
+    def test_instance(self):
+        config = basic_conf.BasicAuthConfig("sanger.ac.uk", "never")
+
+        self.assertEqual(config.url(), "http://sanger.ac.uk")
+        self.assertIsNone(config.cache())
+
+
+if __name__ == "__main__":
+    unittest.main()
