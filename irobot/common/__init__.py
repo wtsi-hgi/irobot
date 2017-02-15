faa9649d6ac0b64285286edc295000db9464f8ce
Christopher Harrison <ch12@sanger.ac.uk>
2017-02-15 15:26:21 +0000

    Removed UTC timezone to use version from stdlib

--- irobot/common/__init__.py
+++ irobot/common/__init__.py
@@ -2,4 +2,3 @@ from irobot.common.canonical_path import canonical_path
 from irobot.common.datetime_arithmetic import multiply_timedelta, add_years
 from irobot.common.parse_human_size import parse_human_size
 from irobot.common.listener import Listener
-from irobot.common.utc import utc
