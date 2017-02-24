5bdafcc9fa8e1c861166270a33306ef19d20ea01
Christopher Harrison <ch12@sanger.ac.uk>
2017-02-24 16:03:37 +0000

    Thread safe(ish) SQLite connection

---
+++ irobot/precache/_sqlite.py
@@ -0,0 +1,139 @@
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Author: Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import re
+import sqlite3
+from datetime import timedelta
+from enum import Enum
+from threading import Lock
+
+
+# This is a bit of a dirty hack! We simply search SQL statements for
+# keywords that indicate that they may be statements that perform a
+# write operation. Unfortunately, we can't just search for SELECT
+# statements, because they can appear elsewhere; similarly, we can't
+# check just the first word because of WITH statements and BEGIN
+# transaction blocks. It will trigger false positives whenever any of
+# the keywords are part of a simple SELECT statement, but the only
+# effect will be to forcibly serialise that SELECT.
+_potentially_writes = re.compile("|".join(map(lambda alt: rf"(?: \b {alt} \b )", [
+    r"BEGIN (?: \s+ (?: DEFERRED | IMMEDIATE | EXCLUSIVE ))? (?: \s+ TRANSACTION )?",
+    r"(?: COMMIT | END ) (?: \s+ TRANSACTION )?",
+    r"ROLLBACK (?: \s+ TRANSACTION )?",
+    r"(?: INSERT | REPLACE | (?: INSERT \s+ OR \s+ (?: REPLACE | ROLLBACK | ABORT | FAIL | IGNORE ))) \s+ INTO \s+ (?: (?: \w+ \. )? \w+ )",
+    r"PRAGMA \s+ (?: (?: \w+ \. )? \w+ )",
+    r"REINDEX",
+    r"SAVEPOINT \s+ \w+",
+    r"RELEASE (?: \s+ SAVEPOINT )? \s+ \w+",
+    r"UPDATE (?: \s+ OR \s+ (?: ROLLBACK | ABORT | REPLACE | FAIL | IGNORE ))? \s+ (?: (?: \w+ \. )? \w+ ) \s+ SET",
+    r"VACUUM",
+    r"CREATE (?: \s+ UNIQUE )? \s+ INDEX (?: \s+ IF \s+ NOT \s+ EXISTS )? \s+ (?: (?: \w+ \. )? \w+ ) \s+ ON \s+ \w+",
+    r"CREATE (?: \s+ TEMP (?: ORARY )?)? \s+ (?: TABLE | TRIGGER | VIEW ) (?: \s+ IF \s+ NOT \s+ EXISTS )? \s+ (?: (?: \w+ \. )? \w+ )",
+    r"CREATE \s+ VIRTUAL \s+ TABLE (?: \s+ IF \s+ NOT \s+ EXISTS )? \s+ (?: (?: \w+ \. )? \w+ ) \s+ USING \s+ \w+",
+    r"DELETE \s+ FROM \s+ (?: (?: \w+ \. )? \w+ )",
+    r"DROP \s+ (?: INDEX | TABLE | TRIGGER | VIEW ) (?: IF \s+ EXISTS )? \s+ (?: (?: \w+ \. )? \w+)",
+    r"ALTER \s+ TABLE \s+ (?: (?: \w+ \. )? \w+ ) \s+ (?: RENAME | ADD )",
+    r"ANALYZE"
+])), re.VERBOSE | re.IGNORECASE).search
+
+
+class IsolationLevel(Enum):
+    """ SQLite3 isolation levels """
+    AUTOCOMMIT = None
+    DEFERRED = "DEFERRED"
+    IMMEDIATE = "IMMEDIATE"
+    EXCLUSIVE = "EXCLUSIVE"
+
+
+class TypeParser(Enum):
+    """ SQLite3 type parsing constants """
+    NONE = 0
+    DECLTYPES = sqlite3.PARSE_DECLTYPES
+    COLNAMES = sqlite3.PARSE_COLNAMES
+    ALL = sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+
+
+class _ThreadSafeConnection(sqlite3.Connection):
+    """ Subclass SQLite connections such that writes are serialised """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._write_lock = Lock()
+
+    ## Always serialise ################################################
+
+    def commit(self):
+        """ Serialised Connection.commit """
+        with self._write_lock:
+            return super().commit()
+
+    def executemany(self, sql:str, *args, **kwargs) -> sqlite3.Cursor:
+        """
+        Serialised Connection.executemany
+
+        @note    Connection.executemany can only execute DML statements,
+                 hence why we always serialise its calls
+        """
+        with self._write_lock:
+            return super().executemany(sql, *args, **kwargs)
+
+    ## Only serialise for writes #######################################
+
+    def _serialse_op(self, op:str, sql:str, *args, **kwargs) -> sqlite3.Cursor:
+        """
+        Serialise writes for Connection methods
+
+        @param   op              Connection method name (str)
+        @param   sql             SQL statement(s) (str)
+        @param   *args/**kwargs  SQL parameters (if applicable)
+        @return  Cursor (sqlite3.Cursor)
+        """
+        potentially_writing = _potentially_writes(sql)
+        if potentially_writing:
+            self._write_lock.acquire()
+
+        cursor = getattr(super(), op)(sql, *args, **kwargs)
+
+        if potentially_writing:
+            self._write_lock.release()
+
+        return cursor
+
+    def execute(self, sql:str, *args, **kwargs) -> sqlite3.Cursor:
+        return self._serialse_op("execute", sql, *args, **kwargs)
+
+    def executescript(self, sql_script:str) -> sqlite3.Cursor:
+        return self._serialse_op("executescript", sql_script)
+
+
+def connect(database:str,
+            timeout:timedelta = timedelta(seconds=5),
+            detect_types:TypeParser = TypeParser.NONE,
+            isolation_level:IsolationLevel = IsolationLevel.AUTOCOMMIT,
+            cached_statements:int = 100,
+            uri:bool = False) -> _ThreadSafeConnection:
+
+    """ Instantiate a thread-safe connection """
+    return sqlite3.connect(database=database,
+                           timeout=timeout.total_seconds(),
+                           detect_types=detect_types.value,
+                           isolation_level=isolation_level.value,
+                           check_same_thread=False,
+                           factory=_ThreadSafeConnection,
+                           cached_statements=cached_statements,
+                           uri=uri)
---
+++ irobot/test/precache/test_sqlite.py
@@ -0,0 +1,202 @@
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Author: Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import unittest
+from unittest.mock import MagicMock
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+from typing import Tuple
+
+import irobot.precache._sqlite as _sqlite
+
+
+class TestMentalSQLRegEx(unittest.TestCase):
+    def _assert_writes(self, *statements:Tuple[str, ...]):
+        for statement in statements:
+            self.assertIsNotNone(_sqlite._potentially_writes(statement))
+
+    def _assert_no_write(self, *statements:Tuple[str, ...]):
+        for statement in statements:
+            self.assertIsNone(_sqlite._potentially_writes(statement))
+
+    def test_select(self):
+        self._assert_no_write(
+            "select * from foo",
+            "with foo as (select * from bar) select * from foo",
+            "explain query plan select * from foo"
+        )
+
+    def test_transaction_block(self):
+        self._assert_writes(
+            "begin",
+            "begin transaction",
+            "begin deferred",
+            "begin deferred transaction",
+            "begin immediate",
+            "begin immediate transaction",
+            "begin exclusive",
+            "begin exclusive transaction",
+
+            "commit",
+            "commit transaction",
+            "end",
+            "end transaction",
+
+            "rollback",
+            "rollback transaction to savepoint foo"
+        )
+
+    def test_misc(self):
+        self._assert_writes(
+            "pragma foo.bar(123)",
+            "analyze",
+            "vacuum",
+            "savepoint foo",
+            "release foo",
+            "release savepoint foo"
+        )
+
+    def test_table(self):
+        self._assert_writes(
+            "create table foo.bar (quux)",
+            "alter table foo.bar add column quux",
+            "alter table foo.bar rename",
+            "create temp table foo",
+            "create temporary table foo",
+            "create table if not exists foo",
+            "create virtual table foo using bar",
+            "create virtual table if not exists foo using bar",
+            "drop table foo",
+            "drop table if exists foo.bar"
+        )
+
+    def test_index(self):
+        self._assert_writes(
+            "create index foo.bar on quux",
+            "create unique index foo on bar",
+            "create index if not exists foo on quux",
+            "reindex",
+            "drop index foo",
+            "drop index if exists foo.bar"
+        )
+
+    def test_view(self):
+        self._assert_writes(
+            "create view foo",
+            "create view if not exists foo.bar",
+            "create temp view foo",
+            "create temporary view foo.bar",
+            "drop view foo",
+            "drop view if exists foo.bar"
+        )
+
+    def test_trigger(self):
+        self._assert_writes(
+            "create trigger foo",
+            "create trigger if not exists foo.bar",
+            "create temp trigger foo",
+            "create temporary trigger foo.bar",
+            "drop trigger foo",
+            "drop trigger if exists foo.bar"
+        )
+
+    def test_dml(self):
+        self._assert_writes(
+            "insert into foo",
+            "insert into foo.bar",
+            "replace into foo",
+            "insert or replace into foo",
+            "insert or rollback into foo",
+            "insert or abort into foo",
+            "insert or fail into foo",
+            "insert or ignore into foo",
+
+            "update foo set",
+            "update or rollback foo.bar set",
+            "update or abort foo set",
+            "update or replace foo set",
+            "update or fail foo set",
+            "update or ignore foo set",
+
+            "delete from foo",
+            "delete from foo.bar"
+        )
+
+
+class TestThreadSafeConnection(unittest.TestCase):
+    def setUp(self):
+        self._old_lock, _sqlite.Lock = _sqlite.Lock, MagicMock(spec=Lock)
+        self.connection = _sqlite.connect(":memory:")
+
+    def tearDown(self):
+        self.connection.close()
+        _sqlite.Lock = self._old_lock
+
+    def test_connection(self):
+        self.assertFalse(self.connection.in_transaction)
+        self.assertEqual(self.connection.total_changes, 0)
+
+    def test_commit(self):
+        self.connection.commit()
+        self.connection._write_lock.__enter__.assert_called_once()
+        self.connection._write_lock.__exit__.assert_called_once()
+    
+    def test_execute(self):
+        cur = self.connection.execute("select 123")
+        self.assertEqual(cur.fetchone(), (123,))
+        self.connection._write_lock.acquire.assert_not_called()
+
+        self.connection.execute("create table foo(bar)")
+        self.connection._write_lock.acquire.assert_called_once()
+        self.connection._write_lock.release.assert_called_once()
+
+    def test_executemany(self):
+        self.connection.execute("create table foo(bar)")
+        self.connection._write_lock.reset_mock()
+
+        self.connection.executemany("insert into foo(bar) values (?)", [(1,), (2,), (3,)])
+        self.connection._write_lock.__enter__.assert_called_once()
+        self.connection._write_lock.__exit__.assert_called_once()
+
+    def test_executescript(self):
+        self.connection.executescript("select 123; select 456;")
+        self.connection._write_lock.acquire.assert_not_called()
+        
+        self.connection.executescript("create table foo(bar); select * from foo;")
+        self.connection._write_lock.acquire.assert_called_once()
+        self.connection._write_lock.release.assert_called_once()
+
+
+class TestThreadSafeWriting(unittest.TestCase):
+    def test_multithread_write(self):
+        conn = _sqlite.connect(":memory:")
+        conn.execute("create table foo(bar)")
+
+        with ThreadPoolExecutor() as executor:
+            for x in range(10):
+                executor.submit(conn.execute, "insert into foo(bar) values (?)", (x,))
+
+        conn.row_factory = lambda _, row: row[0]
+        self.assertEqual(conn.execute("select bar from foo order by bar").fetchall(), list(range(10)))
+
+        conn.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
