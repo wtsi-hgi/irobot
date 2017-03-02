903f87e1150a575f0da0d027afa5d7344db08d0c
Christopher Harrison <ch12@sanger.ac.uk>
2017-03-02 16:06:47 +0000

    Set DB to exclusive mode

--- irobot/precache/schema.sql
+++ irobot/precache/schema.sql
@@ -18,6 +18,7 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 pragma foreign_keys = ON;
+pragma locking_mode = EXCLUSIVE;
 
 create table if not exists datatypes (
   id           integer  primary key,
