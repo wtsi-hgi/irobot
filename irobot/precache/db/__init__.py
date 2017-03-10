83b682854e2732759a3760625eabc700a15d3812
Christopher Harrison <ch12@sanger.ac.uk>
2017-03-10 10:46:07 +0000

    Export named tuples for tracking DB

--- irobot/precache/db/__init__.py
+++ irobot/precache/db/__init__.py
@@ -1,2 +1,3 @@
-from irobot.precache.db.tracker import TrackingDB, Datatype, Mode, Status
 from irobot.precache.db._exceptions import StatusExists, SwitchoverExists, SwitchoverDoesNotExist, PrecacheExists
+from irobot.precache.db.tracker import (TrackingDB, Datatype, Mode, Status,
+                                        SummaryStat, DataObjectFileStatus)
