de580db6e09abb58f0f35be8df10bd5950aa5ee8
Christopher Harrison <ch12@sanger.ac.uk>
2017-04-07 14:55:07 +0100

    Precache directory handling functions and tests

--- irobot/precache/precache.py
+++ irobot/precache/precache.py
@@ -19,12 +19,49 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 import os
+import shutil
 from typing import Optional
+from uuid import uuid4
 
 from irobot.config.precache import PrecacheConfig
 from irobot.logging import LogWriter
 from irobot.precache._checksummer import ChecksumStatus, Checksummer
 from irobot.precache.db import TrackingDB
+
+
+def _new_precache_dir(precache_path:str) -> str:
+    """
+    Create a new precache directory path (i.e., just the string), by
+    appending the precache base directory to a UUID4 split into 2-byte
+    segments (i.e., so each level contains a maximum of 256 directories)
+
+    @param   precache_path  Precache base path (string)
+    @return  Precache directory path (string)
+    """
+    segments = map(lambda x: "".join(x), zip(*[iter(uuid4().hex)] * 2))
+    return os.path.join(precache_path, *segments)
+
+def _create_precache_dir(precache_dir:str) -> None:
+    """
+    Create a given precache directory on the filesystem (mkdir -p)
+
+    @param   precache_dir  Full precache directory
+    @note    Use with _new_precache_dir separately so the tracking DB
+             can check for collisions
+    @note    The directory is fully accessible to the user and only
+             readable by the group
+    """
+    os.makedirs(precache_dir, mode=0o750)
+
+def _delete_precache_dir(precache_dir:str) -> None:
+    """
+    Delete the top-level precache directory and its contents from the
+    filesystem
+
+    @param   precache_dir  Full precache directory
+    @note    e.g., if deleting /foo/bar/quux, only quux will be removed
+    """
+    shutil.rmtree(precache_dir)
 
 
 class Precache(LogWriter):
