c5d1c8fe3ea9a75fad488f7489c781807ef517da
Christopher Harrison <ch12@sanger.ac.uk>
2017-05-10 16:35:25 +0100

    Added `unknown` state to AsyncTaskStatus enum

--- irobot/common/types.py
+++ irobot/common/types.py
@@ -19,4 +19,4 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from enum import Enum
 
-AsyncTaskStatus = Enum("AsyncTaskStatus", "queued started finished failed")
+AsyncTaskStatus = Enum("AsyncTaskStatus", "queued started finished unknown failed")
