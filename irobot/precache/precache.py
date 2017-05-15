ca0274a2b1d3611b73ae650c8117721fade4e6b0
Christopher Harrison <ch12@sanger.ac.uk>
2017-05-15 16:48:15 +0100

    The humble beginnings of the precache controller monster...

--- irobot/precache/precache.py
+++ irobot/precache/precache.py
@@ -17,5 +17,95 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-class Precache(object):
-    pass
+import logging
+import os
+from datetime import datetime
+from typing import Any, Dict, Optional
+
+# A *lot* of moving parts come together here...
+from irobot.common import AsyncTaskStatus
+from irobot.config.precache import PrecacheConfig
+from irobot.irods import AVU, Metadata, MetadataJSONDecoder, MetadataJSONEncoder, iRODS
+from irobot.logging import LogWriter
+from irobot.precache._checksummer import Checksummer
+from irobot.precache._dir_utils import new_name, create, delete
+from irobot.precache._entity import DataObject, Entity
+from irobot.precache._types import ByteRange, ByteRangeChecksum
+from irobot.precache.db import (TrackingDB,
+                                Datatype, Mode, Status,
+                                SummaryStat, DataObjectFileStatus,
+                                StatusExists, SwitchoverExists, SwitchoverDoesNotExist, PrecacheExists)
+
+
+# PRECACHE PLAN
+# =============
+# ...because it's super-complicated, so better to have a target to work
+# towards than blindly stabbing in the dark :P
+#
+# TODO Write this out completely before implementing anything!!
+#
+# How to Calculate ETA
+# --------------------
+# Estimate is calculated based on the size of data ahead of it in the
+# queue (including things currently being processed, thus making it an
+# over-estimate), divided by the number of workers, plus the size of the
+# data in question:
+#
+#   ETA = Now + (((<Size Ahead> / Workers) + <DO Size>) / Rate)
+#
+# FIXME We shouldn't base from now, but rather the start times of async
+# processes
+
+
+class _WorkerMetrics(object):
+    """ Simple container for worker metrics """
+    def __init__(self, workers:int, rate:Optional[SummaryStat] = None) -> None:
+        self._workers = workers
+        self.rate = rate
+
+    @property
+    def workers(self):
+        return self._workers
+
+
+class Precache(LogWriter):
+    """ High-level precache management interface """
+    def __init__(self, precache_config:PrecacheConfig, irods:iRODS, logger:Optional[logging.Logger] = None) -> None:
+        """
+        Constructor
+
+        @param   precache_config  Precache configuration (PrecacheConfig)
+        @param   irods            iRODS interface (iRODS)
+        @param   logger           Logger
+        """
+        super().__init__(logger=logger)
+        self.log(logging.DEBUG, "Starting precache")
+
+        self.config = precache_config
+
+        db_in_precache = os.path.commonpath([precache_config.location, precache_config.index]) == precache_config.location
+        self.tracker = TrackingDB(precache_config.index, db_in_precache, logger)
+
+        _interface_map:Dict[Datatype, Any] = {}
+
+        self.irods = _interface_map[Datatype.data] = irods
+        # TODO We need an iRODS listener that updates the state of the
+        # precache appropriately as-and-when iRODS broadcasts come in
+
+        self.checksummer = _interface_map[Datatype.checksums] = Checksummer(precache_config, logger)
+        # TODO We also need a checksummer listener, for the same reason
+
+        # Statistics for workers
+        self.worker_stats = {
+            datatype: _WorkerMetrics(_interface_map[datatype], rate)
+            for datatype, rate in self.tracker.production_rates.items()
+        }
+
+        if self.config.expiry(datetime.utcnow()):
+            # TODO Cache expiry invalidation: We need a function that is
+            # scheduled to run periodically to delete old data when we
+            # have a non-trivial expiration policy
+            pass
+
+        # TODO We need a function that cleans-up/sanitises bad state on
+        # initialisation
