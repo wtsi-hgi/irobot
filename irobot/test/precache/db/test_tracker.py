965b6f4b986f07af080d4cf8240350465fb7efee
Christopher Harrison <ch12@sanger.ac.uk>
2017-05-19 15:10:02 +0100

    Updated DB magic tests

--- irobot/test/precache/db/test_tracker.py
+++ irobot/test/precache/db/test_tracker.py
@@ -27,8 +27,8 @@ from datetime import datetime, timedelta
 from tempfile import NamedTemporaryFile
 
 import irobot.precache.db.tracker as _tracker
-from irobot.precache.db import TrackingDB, Datatype, Mode, Status
-from irobot.precache.db._exceptions import PrecacheExists, StatusExists, SwitchoverExists, SwitchoverDoesNotExist
+from irobot.precache.db import TrackingDB, Datatype, Status
+from irobot.precache.db._exceptions import PrecacheExists, StatusExists
 
 
 class TestMisc(unittest.TestCase):
@@ -46,70 +46,28 @@ class TestDBMagic(unittest.TestCase):
     def setUp(self):
         self.tracker = TrackingDB(":memory:")
 
-        # Create a dummy record; this belongs to the auto_request
-        # trigger test, but it's also common to everything else
-        self.tracker._exec("insert into do_requests(irods_path, precache_path) values (\"foo\", \"bar\")")
+        # Create a dummy record
+        self.tracker._exec("insert into data_objects(irods_path, precache_path) values (\"foo\", \"bar\")")
         self.do_id, = self.tracker._exec("select id from data_objects where irods_path = ?", ("foo",)).fetchone() or (None,)
 
-    def test_auto_request_trigger(self):
-        self.assertIsNotNone(self.do_id)
-
-        precache_path, = self.tracker._exec("select precache_path from do_modes where data_object = ? and mode = 1", (self.do_id,)).fetchone() or (None,)
-        self.assertEqual(precache_path, "bar")
-
-    def test_auto_first_access_trigger(self):
-        last_access_sql = "select last_access from last_access where data_object = ?"
-
-        first_access, = self.tracker._exec(last_access_sql, (self.do_id,)).fetchone() or (None,)
-        self.assertIsNotNone(first_access)
-
-        self.tracker._exec("insert into do_modes(data_object, mode, precache_path) values (?, 2, \"quux\")", (self.do_id,))
-
-        no_change, = self.tracker._exec(last_access_sql, (self.do_id,)).fetchone() or (None,)
-        self.assertEqual(first_access, no_change)
-
     def test_auto_first_status_trigger(self):
-        dom_id, = self.tracker._exec("select id from do_modes where data_object = ? and mode = 1", (self.do_id,)).fetchone()
-
         for datatype in Datatype:
-            status, = self.tracker._exec("select status from status_log where dom_file = ? and datatype = ?", (dom_id, datatype)).fetchone() or (None,)
+            status, = self.tracker._exec("select status from status_log where data_object = ? and datatype = ?", (self.do_id, datatype)).fetchone() or (None,)
             self.assertEqual(status, Status.requested)
 
     def test_cascade_delete_data_object(self):
-        self.tracker._exec("insert into do_modes(data_object, mode, precache_path) values (?, 2, \"quux\")", (self.do_id,))
-
-        doms, = self.tracker._exec("select count(*) from do_modes where data_object = ?", (self.do_id,)).fetchone()
-        self.assertEqual(doms, 2)
-
-        last_access, = self.tracker._exec("select last_access from last_access where data_object = ?", (self.do_id,)).fetchone() or (None,)
-        self.assertIsNotNone(last_access)
+        for datatype, size in (Datatype.data, 123), (Datatype.metadata, 456), (Datatype.checksums, 789):
+            self.tracker._exec("""
+                insert into data_sizes (data_object, datatype, size)
+                                values (?, ?, ?);
+            """, (self.do_id, datatype, size))
 
         self.tracker._exec("delete from data_objects where id = ?", (self.do_id,))
 
-        doms, = self.tracker._exec("select count(*) from do_modes where data_object = ?", (self.do_id,)).fetchone()
-        self.assertEqual(doms, 0)
-
-        last_access, = self.tracker._exec("select last_access from last_access where data_object = ?", (self.do_id,)).fetchone() or (None,)
-        self.assertIsNone(last_access)
-
-    def test_cascade_delete_data_object_mode(self):
-        dom_id, = self.tracker._exec("select id from do_modes where data_object = ? and mode = 1", (self.do_id,)).fetchone()
-
-        self.tracker._exec("insert into data_sizes(dom_file, datatype, size) values (?, 1, 123)", (dom_id,))
-
-        ds_count, = self.tracker._exec("select count(*) from data_sizes where dom_file = ?", (dom_id,)).fetchone()
-        self.assertEqual(ds_count, 1)
-
-        stat_count, = self.tracker._exec("select count(*) from status_log where dom_file = ?", (dom_id,)).fetchone()
-        self.assertEqual(stat_count, len(Datatype))
-
-        self.tracker._exec("delete from do_modes where id = ?", (dom_id,))
-
-        ds_count, = self.tracker._exec("select count(*) from data_sizes where dom_file = ?", (dom_id,)).fetchone()
-        self.assertEqual(ds_count, 0)
-
-        stat_count, = self.tracker._exec("select count(*) from status_log where dom_file = ?", (dom_id,)).fetchone()
-        self.assertEqual(stat_count, 0)
+        sizes, = self.tracker._exec("select count(*) from data_sizes where data_object = ?", (self.do_id,)).fetchone()
+        statuses, = self.tracker._exec("select count(*) from status_log where data_object = ?", (self.do_id,)).fetchone()
+        self.assertEqual(sizes, 0)
+        self.assertEqual(statuses, 0)
 
     def test_reset_bad_state_on_init(self):
         with NamedTemporaryFile() as temp_db:
@@ -117,17 +75,14 @@ class TestDBMagic(unittest.TestCase):
             (before_count,), *_ = before._exec("""
                 begin immediate transaction;
 
-                insert into do_requests (irods_path, precache_path)
-                                 values ("foo", "bar");
+                insert into data_objects (irods_path, precache_path)
+                                  values ("foo", "bar");
 
-                insert into status_log (dom_file, datatype, status)
-                    select     do_modes.id,
+                insert into status_log (data_object, datatype, status)
+                    select     data_objects.id,
                                datatypes.id,
                                2
                     from       data_objects
-                    join       do_modes
-                    on         do_modes.data_object    = data_objects.id
-                    and        do_modes.mode           = 1
                     cross join datatypes
                     where      data_objects.irods_path = "foo";
 
@@ -148,10 +103,6 @@ class TestDBMagic(unittest.TestCase):
 
             sanity_check, = after._exec("select count(*) from current_status where status = 1").fetchone()
             self.assertEqual(sanity_check, len(Datatype))
-
-    def test_denormalised_state(self):
-        # TODO
-        pass
 
 
 class TestTrackingDB(unittest.TestCase):
