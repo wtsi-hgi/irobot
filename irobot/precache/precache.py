647de04590134a142324c28b0f2ffc94c30072a7
Christopher Harrison <ch12@sanger.ac.uk>
2017-06-07 17:30:49 +0100

    Precache invalidation and garbage collection

--- irobot/precache/precache.py
+++ irobot/precache/precache.py
@@ -17,19 +17,21 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
+import atexit
 import logging
 import os
 from datetime import datetime, timedelta
-from typing import Any, Dict, Optional, Tuple
+from threading import Lock, Timer
+from typing import Dict, List, Optional, Tuple
 
 # A *lot* of moving parts come together here...
-from irobot.common import AsyncTaskStatus
+from irobot.common import AsyncTaskStatus, WorkerPool
 from irobot.config.precache import PrecacheConfig
 from irobot.irods import AVU, Metadata, MetadataJSONDecoder, MetadataJSONEncoder, iRODS
 from irobot.logging import LogWriter
 from irobot.precache._checksummer import Checksummer
 from irobot.precache._dir_utils import new_name, create, delete
-from irobot.precache._entity import DataObject, Entity
+from irobot.precache._do import DataObject
 from irobot.precache._types import ByteRange, ByteRangeChecksum
 from irobot.precache.db import (TrackingDB, Datatype, Status,
                                 SummaryStat, DataObjectFileStatus,
@@ -43,7 +45,7 @@ class _WorkerMetrics(object):
         self.rate = rate
 
     @property
-    def workers(self):
+    def workers(self) -> int:
         return self._workers
 
 
@@ -92,7 +94,14 @@ class Precache(LogWriter):
         db_in_precache = os.path.commonpath([precache_config.location, precache_config.index]) == precache_config.location
         self.tracker = TrackingDB(precache_config.index, db_in_precache, logger)
 
-        _interface_map:Dict[Datatype, Any] = {}
+        # In-memory representation of the precache
+        self.data_objects:Dict[str, DataObject] = {}
+        self._do_lock = Lock()
+
+        # TODO We need a function that cleans-up/sanitises bad state on
+        # initialisation
+
+        _interface_map:Dict[Datatype, WorkerPool] = {}
 
         self.irods = _interface_map[Datatype.data] = irods
         # TODO We need an iRODS listener that updates the state of the
@@ -103,15 +112,119 @@ class Precache(LogWriter):
 
         # Statistics for workers
         self.worker_stats = {
-            datatype: _WorkerMetrics(_interface_map[datatype], rate)
+            datatype: _WorkerMetrics(_interface_map[datatype].workers, rate)
             for datatype, rate in self.tracker.production_rates.items()
         }
 
-        if self.config.expiry(datetime.utcnow()):
-            # TODO Cache expiry invalidation: We need a function that is
-            # scheduled to run periodically to delete old data when we
-            # have a non-trivial expiration policy
-            pass
+        # Garbage collection setup
+        self.temporal_gc = self.config.expiry(datetime.utcnow()) is not None
+        self.capacity_gc = self.config.size is not None and self.config.age_threshold is not None
 
-        # TODO We need a function that cleans-up/sanitises bad state on
-        # initialisation
+        if self.temporal_gc:
+            # FIXME? This is a bit contrived :P
+            now = datetime.utcnow()
+            self._gc_period = (self.config.expiry(now) - now) / 2
+
+            self._schedule_temporal_gc()
+            atexit.register(self._gc_timer.cancel)
+
+    def _schedule_temporal_gc(self) -> None:
+        """ Initialise and start the GC timer """
+        self._gc_timer = Timer(self._gc_period.total_seconds(), self._gc)
+        self._gc_timer.daemon = True
+        self._gc_timer.start()
+
+    def __del__(self) -> None:
+        """ Cancel any running GC timer on python GC """
+        if self.temporal_gc and self._gc_timer.is_alive():
+            self._gc_timer.cancel()
+
+    def _gc(self) -> None:
+        """ Garbage collect invalidated entries from the precache """
+        with self._do_lock:
+            self.log(logging.DEBUG, "Running precache garbage collection")
+            gc_time = datetime.utcnow()
+
+            # Data objects marked as invalid or expired (if appropriate)
+            to_gc = [
+                irods_path
+                for irods_path, do in self.data_objects.items()
+                if do.invalid or \
+                   (self.temporal_gc and gc_time > self.config.expiry(do.last_accessed))
+            ]
+
+            # Garbage collect
+            for irods_path in to_gc:
+                self.log(logging.DEBUG, f"Garbage collecting {irods_path} from precache")
+
+                do = self.data_objects[irods_path]
+                do_id = self.tracker.get_data_object_id(irods_path)
+
+                self.tracker.delete_data_object(do_id)
+                delete(do.precache_path)
+                del self.data_objects[irods_path]
+
+        if self.temporal_gc:
+            self._schedule_temporal_gc()
+
+    def accommodate(self, accommodation:int) -> None:
+        """
+        Attempt to invalidate data objects to fulfil the specified space
+        requirements. Data objects will only be invalidated if the
+        accommodation can be fulfilled (in which case, the garbage
+        collector will be invoked automatically), otherwise a
+        PrecacheFull exception will be raised.
+
+        @note    This function should only be called if capacity GC is
+                 enabled (i.e., both the precache size and age threshold
+                 are limited).
+
+        @note    Data objects are invalidated, oldest first, until the
+                 accommodation is reached.
+
+        @note    If the accommodation can't be reached, then
+                 invalidation is cancelled and a PrecacheFull exception
+                 will be raised.
+
+        @param   accommodation  Required precache space in bytes (int)
+        """
+        with self._do_lock:
+            invalidation_time = datetime.utcnow()
+
+            to_invalidate:List[str] = []
+            invalidatable = sorted([
+                (irods_path, do.last_accessed, do.metadata.size)
+                for irods_path, do in self.data_objects.items()
+                if  invalidation_time - do.last_accessed > self.config.age_threshold
+            ], key=lambda x: x[1].last_accessed, reverse=True)
+
+            freed_space:int = 0
+            while freed_space < accommodation and invalidatable:
+                irods_path, _, size = invalidatable.pop()
+                to_invalidate.append(irods_path)
+                freed_space += size
+
+            if freed_space < accommodation:
+                raise PrecacheFull(f"Precache cannot accommodate {accommodation} bytes")
+
+            for do in to_invalidate:
+                self.data_objects[do].invalid = True
+
+        if to_invalidate:
+            self._gc()
+
+    def invalidate(self, irods_path:str) -> None:
+        """
+        Manually invalidate a data object and call the GC
+
+        @param   irods_path  Data object iRODS path (string)
+        """
+        call_gc = False
+
+        with self._do_lock:
+            if irods_path in self.data_objects:
+                call_gc = True
+                self.data_objects[irods_path].invalid = True
+
+        if call_gc:
+            self._gc()
