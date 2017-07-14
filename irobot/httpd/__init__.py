28432a7f27c347bed68946327f94c32bc6ebe26b
Christopher Harrison <ch12@sanger.ac.uk>
2017-07-14 15:39:34 +0100

    Beginnings of API server
    Experimenting with Python's asyncio event loop... Hopefully this will end well :P

---
+++ irobot/httpd/__init__.py
@@ -0,0 +1,1 @@
+from irobot.httpd.server import APIServer
---
+++ irobot/httpd/server.py
@@ -0,0 +1,90 @@
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Author: Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import asyncio
+import logging
+from threading import Lock, Thread
+from typing import Optional
+
+import aiohttp
+
+from irobot.config.httpd import HTTPdConfig
+from irobot.logging import LogWriter
+
+
+class APIServer(LogWriter):
+    """ HTTP API server interface """
+    _loop:asyncio.AbstractEventLoop
+
+    def __init__(self, httpd_config:HTTPdConfig, logger:Optional[logging.Logger] = None) -> None:
+        """
+        Constructor: Start the event loop in a separate thread, which
+        listens for and serves API requests
+
+        @param   httpd_config  HTTPd configuration (HTTPdConfig)
+        @param   logger        Logger
+        @param   TODO...
+        """
+        super().__init__(logger=logger)
+        self.log(logging.DEBUG, "Starting API server")
+
+        self._loop_lock = Lock()
+        self._loop_lock.acquire()
+
+        # Start the event loop thread
+        self._thread = Thread(target=self._init_loop, daemon=True)
+        self._thread.start()
+
+        with self._loop_lock:
+            # TODO Start the listener on the event loop once it's ready
+            pass
+
+        # We don't need the loop lock any more
+        del self._loop_lock
+
+    def __del__(self) -> None:
+        """ Tidy up after ourselves on Python GC """
+        self.close()
+
+    def _init_loop(self) -> None:
+        """
+        Initialise and start the event loop within the policy context
+        (i.e., "thread", under the default policy)
+
+        @note    Blocking
+        """
+        self._loop = asyncio.new_event_loop()
+        self._loop_lock.release()
+
+        asyncio.set_event_loop(self._loop)
+        self._loop.run_forever()
+
+    def close(self) -> None:
+        """
+        Graceful shutdown:
+        * Close HTTPd listener
+        * Stop the event loop and block until the thread terminates
+
+        @note    Blocking
+        """
+        self.log(logging.DEBUG, "Shutting down API server")
+
+        # TODO Close HTTPd listener
+        self._loop.call_soon_threadsafe(self._loop.stop)
+        self._thread.join()
