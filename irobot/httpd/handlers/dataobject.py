f6b870ae61b15a5033bcab936666b1dca8d99ae2
Christopher Harrison <ch12@sanger.ac.uk>
2017-08-08 15:21:06 +0100

    Data object endpoint delegation by request method

--- irobot/httpd/handlers/dataobject.py
+++ irobot/httpd/handlers/dataobject.py
@@ -19,11 +19,28 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import re
 import os
+from typing import Dict
 
 from aiohttp.web import Request, Response
 
+from irobot.httpd._common import HandlerT
 from irobot.httpd._error import error_factory
 from irobot.httpd.handlers import _decorators as request
+
+
+async def _todo(req:Request) -> Response:
+    """ Placeholder handler """
+    irods_path = req["irobot_irods_path"]
+    raise NotImplementedError(f"Watch this space... {irods_path}")
+
+
+# Method -> Handler delegation table
+_delegates:Dict[str, HandlerT] = {
+    "GET":    _todo,
+    "HEAD":   _todo,
+    "POST":   _todo,
+    "DELETE": _todo
+}
 
 
 @request.allow("GET", "HEAD", "POST", "DELETE")
@@ -44,4 +61,5 @@ async def data_object(req:Request) -> Response:
         raise error_factory(404, f"No such data object {irods_path}; "
                                   "must be at least one collection deep.")
 
-    raise NotImplementedError(f"Watch this space... {irods_path}")
+    req["irobot_irods_path"] = irods_path
+    return await _delegates[req.method](req)
