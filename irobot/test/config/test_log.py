65f4132cfc3a5f0e2970262f28a6c7ca33780191
Christopher Harrison <ch12@sanger.ac.uk>
2017-08-10 10:34:24 +0100

    Fixed tests for logging config

--- irobot/test/config/test_log.py
+++ irobot/test/config/test_log.py
@@ -21,18 +21,18 @@ import logging
 import unittest
 from configparser import ParsingError
 
-import irobot.config.log as log
+import irobot.config._log as log
 
 
 class TesLoggingConfig(unittest.TestCase):
     def test_output_parsing(self):
-        canon_output = log._canon_output
+        canon_output = log.output
 
         self.assertIsNone(canon_output("STDERR"))
         self.assertEqual(canon_output("/foo.log"), "/foo.log")
 
     def test_level_parsing(self):
-        canon_level = log._canon_level
+        canon_level = log.level
 
         self.assertRaises(ParsingError, canon_level, "foo")
         self.assertEqual(canon_level("debug"),    logging.DEBUG)
@@ -40,14 +40,6 @@ class TesLoggingConfig(unittest.TestCase):
         self.assertEqual(canon_level("warning"),  logging.WARNING)
         self.assertEqual(canon_level("error"),    logging.ERROR)
         self.assertEqual(canon_level("critical"), logging.CRITICAL)
-
-    def test_instance(self):
-        config = log.LoggingConfig("/var/log/irobot.log", "debug")
-
-        self.assertEqual(config.output, "/var/log/irobot.log")
-        self.assertEqual(config.level, logging.DEBUG)
-        self.assertRegex(str(config), r"output = /var/log/irobot.log")
-        self.assertRegex(str(config), r"level = debug")
 
 
 if __name__ == "__main__":
