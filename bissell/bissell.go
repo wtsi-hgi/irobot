b48ea76d10edd4ff8cb4fd1aca1391f799a27ccf
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-08-23 10:52:12 +0100

    add additional endpoints according to iRobot spec, mostly unimplemented

--- bissell/bissell.go
+++ bissell/bissell.go
@@ -2,14 +2,16 @@ package main
  
 import (
     "encoding/json"
+    "fmt"
     "log"
     "net/http"
     "time"
 
+    "github.com/golang/gddo/httputil"
     "github.com/gorilla/mux"
 )
 
-type Error struct {
+type HttpError struct {
 	Status string `json:"status"`
 	Reason string `json:"reason"`
 	Description string `json:"description"`
@@ -43,15 +45,82 @@ type StatusIrods struct {
 	DownloadRate StatusRate `json:"download_rate"`
 }
 
-var status Status
-
-func GetStatusEndpoint(w http.ResponseWriter, req *http.Request) {
-	json.NewEncoder(w).Encode(status)
+type ManifestEntry struct {
+	Path string `json:"path"`
+	Availability ManifestEntryAvailability `json:"availability"`
+	LastAccessed time.Time `json:"last_accessed"`
+	Contention int `json:"contention"`
 }
+
+type ManifestEntryAvailability struct {
+	Data string `json:"data"`
+	Metadata string `json:"metadata"`
+	Checksums string `json:"checksums"`
+}
+
+func HandleError(w http.ResponseWriter, req *http.Request, code int, reason string, desc string) {
+        status := http.StatusText(code)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if req.Method == http.MethodGet {
+		httpErr := HttpError{Status: status, Reason: reason, Description: desc}
+		json.NewEncoder(w).Encode(httpErr)
+	}
+}
+
+func GetHeadStatusEndpoint(w http.ResponseWriter, req *http.Request) {
+	status := Status{AuthenticatedUser: "username", Connections: StatusConnections{}, Precache: StatusPrecache{}, Irods: StatusIrods{}}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if req.Method == http.MethodGet {
+		json.NewEncoder(w).Encode(status)
+	}
+}
+
+func GetHeadConfigEndpoint(w http.ResponseWriter, req *http.Request) {
+	HandleError(w, req, http.StatusNotImplemented, "config endpoint is not implemented", "nothing to see here.")
+}
+
+func GetHeadManifestEndpoint(w http.ResponseWriter, req *http.Request) {
+	manifest := []ManifestEntry{}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if req.Method == http.MethodGet {
+		json.NewEncoder(w).Encode(manifest)
+	}
+}
+
+func GetHeadDataObject(w http.ResponseWriter, req *http.Request) {
+	acceptable := []string{"application/octet-stream", "application/vnd.irobot.metadata+json"}
+	contentType := httputil.NegotiateContentType(req, acceptable, "")
+	if contentType == "" {
+		HandleError(w, req, http.StatusNotAcceptable, fmt.Sprintf("Please accept one of the supported content types: %v", acceptable), "You specified an Accept header that does not include any of the supported content types.")
+	}
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(http.StatusOK)
+	if req.Method == http.MethodGet {
+		json.NewEncoder(w).Encode(contentType)
+	}
+}
+
+
+func PostDataObject(w http.ResponseWriter, req *http.Request) {
+	HandleError(w, req, http.StatusInsufficientStorage, "Precache not implemented", "Precache/cache management functionality not implemented in this server. Please proceed with request without explicit caching.")
+}
+
+
+func DeleteDataObject(w http.ResponseWriter, req *http.Request) {
+	HandleError(w, req, http.StatusNotFound, "Precache not implemented", "Precache/cache management functionality not implemented in this server, so there is no need to explicitly delete anything.")
+}
+
 
 func main() {
 	router := mux.NewRouter()
-	status = Status{AuthenticatedUser: "username", Connections: StatusConnections{}, Precache: StatusPrecache{}, Irods: StatusIrods{}}
-	router.HandleFunc("/status", GetStatusEndpoint).Methods("GET")
+	router.HandleFunc("/status", GetHeadStatusEndpoint).Methods("GET", "HEAD")
+	router.HandleFunc("/config", GetHeadConfigEndpoint).Methods("GET", "HEAD")
+	router.HandleFunc("/manifest", GetHeadConfigEndpoint).Methods("GET", "HEAD")
+	router.PathPrefix("/").HandlerFunc(GetHeadDataObject).Methods("GET", "HEAD")
+	router.PathPrefix("/").HandlerFunc(PostDataObject).Methods("POST")
+	router.PathPrefix("/").HandlerFunc(DeleteDataObject).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
