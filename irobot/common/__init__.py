fd2c546a93fe485ce9211ff1b5ec1eea9964af6a
Christopher Harrison <ch12@sanger.ac.uk>
2017-08-24 14:15:51 +0100

    Common byte range type

--- irobot/common/__init__.py
+++ irobot/common/__init__.py
@@ -1,4 +1,4 @@
 from irobot.common.constants import ISO8601_UTC
 from irobot.common.datetime_arithmetic import add_years
 from irobot.common.listenable import Listenable
-from irobot.common.types import AsyncTaskStatus, DataObjectState, SummaryStat, WorkerPool
+from irobot.common.types import AsyncTaskStatus, DataObjectState, ByteRange, SummaryStat, WorkerPool
--- irobot/common/types.py
+++ irobot/common/types.py
@@ -20,11 +20,17 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 from abc import ABCMeta, abstractmethod
 from enum import IntEnum
 from numbers import Number
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 
 AsyncTaskStatus = IntEnum("AsyncTaskStatus", "queued started finished unknown failed")
 DataObjectState = IntEnum("DataObjectState", "data metadata checksums")
+
+class ByteRange(NamedTuple):
+    """ Byte range, inclusive, with optional checksum """
+    start: int
+    finish: int
+    checksum: Optional[str] = None
 
 class SummaryStat(NamedTuple):
     """ Tuple of arithmetic mean and standard error """
