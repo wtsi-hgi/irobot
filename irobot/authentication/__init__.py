d75e3048de41eb556a3505864e9473d19b4eda8e
Christopher Harrison <ch12@sanger.ac.uk>
2017-09-18 16:50:16 +0100

    Export AuthenticatedUser in module's public namespace

--- irobot/authentication/__init__.py
+++ irobot/authentication/__init__.py
@@ -1,4 +1,4 @@
-from irobot.authentication._base import BaseAuthHandler
+from irobot.authentication._base import AuthenticatedUser, BaseAuthHandler
 from irobot.authentication.basic import HTTPBasicAuthHandler
 from irobot.authentication.arvados import ArvadosAuthHandler
 from irobot.authentication.parser import HTTPAuthMethod, ParseError, auth_parser
