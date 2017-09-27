f4c565f9194074448f55b58e2099aac3fa8d04ed
Christopher Harrison <ch12@sanger.ac.uk>
2017-09-27 12:54:43 +0100

    Caching tests for HTTP authentication handler base class
    FIXME async code seems to be impervious to monkey patching...which is
    making it awkward to test :(

--- irobot/test/authentication/test_http.py
+++ irobot/test/authentication/test_http.py
@@ -20,33 +20,15 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 import unittest
 from unittest.mock import MagicMock, patch
 from datetime import datetime, timedelta
-from threading import Timer
-from typing import Any
 
-from requests import HTTPError, Response, Request, Session
+from aiohttp import ClientResponse
 
-import irobot.authentication._base as base
 import irobot.authentication._http as http
+from irobot.authentication._base import AuthenticatedUser
+from irobot.authentication.parser import HTTPAuthMethod
 from irobot.config import Configuration
 from irobot.config._tree_builder import ConfigValue
-
-
-class _MockHTTPAuthentication(http.HTTPAuthHandler):
-    @property
-    def www_authenticate(self) -> str:
-        return "Mock"
-
-    def parse_auth_header(self, auth_header:str) -> str:
-        if auth_header == "fail":
-            raise ValueError()
-
-        return auth_header
-
-    def auth_request(self, *args) -> http.Request:
-        return http.Request()
-
-    def get_user(self, req:http.Request, res:http.Response) -> str:
-        return "foo"
+from irobot.test.async import async_test
 
 
 class _MockHTTPAuthConfig(Configuration):
@@ -54,78 +36,78 @@ class _MockHTTPAuthConfig(Configuration):
         super().__init__()
         self.add_value("cache", ConfigValue(cache, lambda x: x))
 
+_CONFIG_CACHE   = _MockHTTPAuthConfig(timedelta(minutes=10))
+_CONFIG_NOCACHE = _MockHTTPAuthConfig(None)
 
-@patch("irobot.authentication._base.datetime", spec=True)
-@patch("irobot.authentication._http.Session", spec=True)
-@patch("irobot.authentication._http.Request", spec=True)
-@patch("irobot.authentication._http.Response", spec=True)
-@patch("irobot.authentication._http.Timer", spec=True)
+
+class _MockHTTPAuthenticator(http.BaseHTTPAuthHandler):
+    @property
+    def www_authenticate(self):
+        return "Mock"
+
+    def match_auth_method(self, challenge_response):
+        return True
+
+    def set_handler_parameters(self, challenge_response):
+        return http.HTTPValidatorParameters("foo", "bar")
+
+    async def get_authenticated_user(self, challenge_response, auth_response):
+        return AuthenticatedUser("Testy McTestface")
+
+
 class TestHTTPAuthenticationHandler(unittest.TestCase):
-    def setUp(self):
-        self.config_cache = _MockHTTPAuthConfig(timedelta(minutes=10))
-        self.config_nocache = _MockHTTPAuthConfig(None)
-
-    def test_creation(self, *args):
-        auth_cache = _MockHTTPAuthentication(self.config_cache)
+    def test_constructor(self):
+        auth_cache = _MockHTTPAuthenticator(_CONFIG_CACHE)
         self.assertTrue(hasattr(auth_cache, "_cache"))
 
-        auth_nocache = _MockHTTPAuthentication(self.config_nocache)
+        auth_nocache = _MockHTTPAuthenticator(_CONFIG_NOCACHE)
         self.assertFalse(hasattr(auth_nocache, "_cache"))
 
-    def test_shutdown_with_cache(self, *args):
-        auth = _MockHTTPAuthentication(self.config_cache)
+    @patch("irobot.authentication._http.Timer", spec=True)
+    def test_cached_shutdown(self, *args):
+        auth = _MockHTTPAuthenticator(_CONFIG_CACHE)
 
         auth._cleanup_timer.is_alive.return_value = True
         auth.__del__()
 
         auth._cleanup_timer.cancel.assert_called_once()
 
-    def test_cleanup(self, mock_timer, mock_response, mock_request, mock_session, mock_datetime):
-        auth = _MockHTTPAuthentication(self.config_cache)
+    @patch("irobot.authentication._base.datetime", spec=True)
+    @patch("irobot.authentication._http.Timer", spec=True)
+    def test_cache_cleanup(self, _mock_timer, mock_datetime):
+        auth = _MockHTTPAuthenticator(_CONFIG_CACHE)
 
+        auth_method = HTTPAuthMethod("foo")
         validation_time = mock_datetime.utcnow.return_value = datetime.utcnow()
-        auth._cache["foo"] = http.AuthenticatedUser("foo")
+        auth._cache[auth_method] = AuthenticatedUser("Testy McTestface")
 
         auth._cleanup()
-        self.assertIn("foo", auth._cache)
-        self.assertEqual(auth._cache["foo"].user, "foo")
-        self.assertEqual(auth._cache["foo"].authenticated, validation_time)
+        self.assertIn(auth_method, auth._cache)
+        self.assertEqual(auth._cache[auth_method].user, "Testy McTestface")
+        self.assertEqual(auth._cache[auth_method].authenticated, validation_time)
 
         mock_datetime.utcnow.return_value = validation_time + timedelta(minutes=11)
         auth._cleanup()
         self.assertEqual(auth._cache, {})
 
-    def test_request_validator(self, mock_timer, mock_response, mock_request, mock_session, mock_datetime):
-        auth = _MockHTTPAuthentication(self.config_cache)
+    # FIXME Patching doesn't seem to work for async code; the below test
+    # is still trying to make the HTTP request, regardless of it
+    # supposedly being mocked...
 
-        mock_session().send().status_code = 200
-        self.assertIsInstance(auth._validate_request(mock_request()), MagicMock)
-
-        mock_session().send().status_code = 401
-        self.assertIsNone(auth._validate_request(mock_request()))
-
-        mock_session().send().status_code = 500
-        mock_session().send().raise_for_status.side_effect = HTTPError
-        self.assertRaises(HTTPError, auth._validate_request, mock_request())
-
-    def test_authenticate(self, mock_timer, mock_response, mock_request, mock_session, mock_datetime):
-        auth = _MockHTTPAuthentication(self.config_cache)
-
-        self.assertIsNone(auth.authenticate("fail"))
-
-        validation_time = mock_datetime.utcnow.return_value = datetime.utcnow()
-        mock_session().send().status_code = 200
-        self.assertEqual(auth._cache, {})
-        self.assertTrue(auth.authenticate("foo"))  # Authenticated
-        self.assertIn("foo", auth._cache)
-        self.assertEqual(auth._cache["foo"].user, "foo")
-        self.assertEqual(auth._cache["foo"].authenticated, validation_time)
-        self.assertTrue(auth.authenticate("foo"))  # Authenticated from cache
-
-        # Invalidate cache and now fail authentication
-        mock_datetime.utcnow.return_value = validation_time + timedelta(minutes=11)
-        mock_session().send().status_code = 403
-        self.assertFalse(auth.authenticate("foo"))
+    # @async_test
+    # @patch("irobot.authentication._http.ClientSession", spec=True)
+    # async def test_request_validator(self, mock_client):
+    #     mock_response = MagicMock(spec=ClientResponse)
+    #     mock_client.request.return_value = mock_response
+    #
+    #     auth = _MockHTTPAuthenticator(_CONFIG_NOCACHE)
+    #     params = http.HTTPValidatorParameters("foo", "bar")
+    #
+    #     mock_response.status = 200
+    #     validation_response = await auth._validate_request(params)
+    #     self.assertEqual(validation_response, mock_response)
+    #
+    # FIXME Same problem anticipated for test_authenticate...
 
 
 if __name__ == "__main__":
