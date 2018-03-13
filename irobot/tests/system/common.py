645788240d1aefba4cbc8aad3f98c7bc497e6ce0
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-03-13 11:57:39 +0000

    Refactored common test code to allow creation of standalone iRobot test server.

--- irobot/tests/system/common.py
+++ irobot/tests/system/common.py
@@ -35,7 +35,6 @@ _IROBOT_IMAGE_NAME = "mercury/irobot-test"
 _IROBOT_EXAMPLE_CONFIG_LOCATION = os.path.join(os.path.dirname(os.path.realpath(__file__)), "irobot.conf")
 
 
-
 _DUMMY_VALUE = "dummy-value"
 
 _docker_client = docker.from_env()
@@ -65,65 +64,104 @@ _HttpStatusPuppetServiceController = DockerisedServiceControllerTypeBuilder(
     start_tries=1).build()
 
 
-class TestWithIrodsSingleton(unittest.TestCase, metaclass=ABCMeta):
+class StandaloneIrods:
     """
-    Tests that share an iRODS instance.
+    Standalone iRODS server.
     """
-    @staticmethod
-    def upload_file(irods: IrodsDockerisedService, contents: str) -> str:
-        file_location = f"/{irods.root_user.zone}/{uuid4()}"
-        with iRODSSession(host=irods.host, port=irods.port, user=irods.root_user.username,
-                          password=irods.root_user.password, zone=irods.root_user.zone) as session:
+    @property
+    def service(self) -> IrodsDockerisedService:
+        if self._irods_service is None:
+            with self._irods_lock:
+                if self._irods_service is None:
+                    self._irods_controller = IrodsServiceController()
+                    self._irods_service = self._irods_controller.start_service()
+        return self._irods_service
+
+    @property
+    def configuration_location(self) -> str:
+        if self._irods_environment_location is None:
+            with self._irods_lock:
+                if self._irods_environment_location is None:
+                    _, self._irods_environment_location = self._temp_manager.create_temp_file()
+                    os.remove(self._irods_environment_location)
+            IrodsServiceController.write_connection_settings(self._irods_environment_location, self.service)
+        return self._irods_environment_location
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+        self._temp_manager = TempManager(default_mkstemp_kwargs=dict(dir=MOUNTABLE_TEMP_DIRECTORY))
+        self._irods_lock = Lock()
+        self._irods_controller = None
+        self._irods_service = None
+        self._irods_environment_location = None
+
+    def upload_file(self, contents: str) -> str:
+        """
+        Uploads the given contents to iRODS and return path to data object (file).
+        :param contents: contents to upload
+        :return: path to data object
+        """
+        irods_service = self.service
+        file_location = f"/{irods_service.root_user.zone}/{uuid4()}"
+        with iRODSSession(host=irods_service.host, port=irods_service.port, user=irods_service.root_user.username,
+                          password=irods_service.root_user.password, zone=irods_service.root_user.zone) as session:
             irods_object = session.data_objects.create(file_location)
             with irods_object.open("w") as file:
                 file.write(contents.encode(_IRODS_ENCODING))
         return file_location
 
-    @classmethod
-    def setUpClass(cls):
-        cls._temp_manager = TempManager(default_mkstemp_kwargs=dict(dir=MOUNTABLE_TEMP_DIRECTORY))
-        cls._irods_lock = Lock()
-        cls._irods_controller = None
-        cls._irods_service = None
-        cls._irods_environment_location = None
-
-    @classmethod
-    def tearDownClass(cls):
-        with cls._irods_lock:
-            cls._temp_manager.tear_down()
-            if cls._irods_service is not None:
-                cls._irods_controller.stop_service(cls._irods_service)
-
-    @classmethod
-    def get_irods(cls) -> IrodsDockerisedService:
-        if cls._irods_service is None:
-            with cls._irods_lock:
-                if cls._irods_service is None:
-                    cls._irods_controller = IrodsServiceController()
-                    cls._irods_service = cls._irods_controller.start_service()
-        return cls._irods_service
-
-    @classmethod
-    def get_irods_environment_json_location(cls):
-        if cls._irods_environment_location is None:
-            with cls._irods_lock:
-                if cls._irods_environment_location is None:
-                    _, cls._irods_environment_location = cls._temp_manager.create_temp_file()
-                    os.remove(cls._irods_environment_location)
-            IrodsServiceController.write_connection_settings(cls._irods_environment_location, cls.get_irods())
-        return cls._irods_environment_location
+    def tear_down(self):
+        """
+        Tears down iRODS if setup.
+        """
+        with self._irods_lock:
+            self._temp_manager.tear_down()
+            if self._irods_service is not None:
+                self._irods_controller.stop_service(self._irods_service)
 
 
-class TestWithIrobot(TestWithIrodsSingleton, metaclass=ABCMeta):
+class StandaloneAuthenticationServer:
     """
-    Tests that uses iRobot.
+    Standalone authentication server.
+    """
+    @property
+    def service(self) -> DockerisedService:
+        if self._basic_authentication_service is None:
+            self._basic_authentication_controller = _HttpStatusPuppetServiceController()
+            self._basic_authentication_service = self._basic_authentication_controller.start_service()
+        return self._basic_authentication_service
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+        self._basic_authentication_controller = None
+        self._basic_authentication_service = None
+
+    def tear_down(self):
+        """
+        Tears down authentication server if setup.
+        """
+        if self._basic_authentication_service is not None:
+            self._basic_authentication_controller.stop_service(self._basic_authentication_service)
+
+
+class StandaloneIrobot:
+    """
+    Standalone iRobot server.
     """
     _docker_client = docker.from_env()
     _irobot_built = False
 
     @staticmethod
     def _build_irobot() -> str:
-        if not TestWithIrobot._irobot_built:
+        """
+        Builds iRobot Docker image and returns image name.
+        :return:
+        """
+        if not StandaloneIrobot._irobot_built:
             log_generator =_docker_client.api.build(
                 path=_IROBOT_DOCKER_BUILD_CONTEXT, dockerfile=_IROBOT_DOCKER_BUILD_FILE,
                 tag=_IROBOT_IMAGE_NAME, decode=True)
@@ -132,61 +170,45 @@ class TestWithIrobot(TestWithIrodsSingleton, metaclass=ABCMeta):
                 if len(details) > 0:
                     logger.debug(details)
 
-            TestWithIrobot._irobot_built = True
+                    StandaloneIrobot._irobot_built = True
 
         return _IROBOT_IMAGE_NAME
 
     @property
-    def irods(self) -> IrodsDockerisedService:
-        return self.get_irods()
-
-    @property
-    def irods_environment_json_location(self):
-        return self.get_irods_environment_json_location()
-
-    @property
-    def irobot(self) -> DockerisedService:
+    def service(self) -> DockerisedService:
         if self._irobot_service is None:
-            basic_authentication_url = f"{self.basic_authentication_server.name}:" \
-                                       f"{list(self.basic_authentication_server.ports.keys())[0]}/" \
+            basic_authentication_url = f"{self.authentication_server.service.name}:" \
+                                       f"{list(self.authentication_server.service.ports.keys())[0]}/" \
                                        f"{BASIC_AUTHENTICATION_SUCCESS_STATUS_CODE}"
-            configuration_location = self.create_irobot_configuration(basic_authentication_url)
-            self._irobot_service = self.start_irobot_server(configuration_location, {
-                self.basic_authentication_server.name: self.basic_authentication_server.container_id})
+            configuration_location = self.create_configuration(basic_authentication_url)
+            self._irobot_service = self.start_server(configuration_location, {
+                self.authentication_server.service.name: self.authentication_server.service.container_id})
         return self._irobot_service
 
     @property
     def irobot_url(self) -> str:
-        return f"http://{self.irobot.host}:{self.irobot.port}"
+        return f"http://{self.service.host}:{self.service.port}"
 
-    @property
-    def basic_authentication_server(self) -> DockerisedService:
-        if self._basic_authentication_service is None:
-            self._basic_authentication_controller = _HttpStatusPuppetServiceController()
-            self._basic_authentication_service = self._basic_authentication_controller.start_service()
-        return self._basic_authentication_service
-
-    def setUp(self):
+    def __init__(self, authentication_server: StandaloneAuthenticationServer, irods: StandaloneIrods):
+        """
+        Constructor.
+        :param authentication_server: authentication server to user (life-cycle does not become the responsibility of
+        this class)
+        :param irods: iRODS server (life-cycle does not become the responsibility of this class)
+        """
+        self.authentication_server = authentication_server
+        self.irods = irods
         self._irobot_controller = None
         self._irobot_service = None
-        self._basic_authentication_controller = None
-        self._basic_authentication_service = None
         self._temp_manager = TempManager(default_mkstemp_kwargs=dict(dir=MOUNTABLE_TEMP_DIRECTORY))
 
-    def tearDown(self):
+    def tear_down(self):
+        """
+        Tears down iRobot if setup.
+        """
         if self._irobot_service is not None:
             self._irobot_controller.stop_service(self._irobot_service)
-        if self._basic_authentication_service is not None:
-            self._basic_authentication_controller.stop_service(self._basic_authentication_service)
         self._temp_manager.tear_down()
-
-    def upload_to_irods(self, contents: str) -> str:
-        """
-        Uploads the given content to iRODS as a data object.
-        :param contents: the contents of the data object that will be created
-        :return:
-        """
-        return TestWithIrodsSingleton.upload_file(self.irods, contents)
 
     def request_data(self, data_object_path: str, irobot_url: str=None) -> Optional[str]:
         """
@@ -215,24 +237,7 @@ class TestWithIrobot(TestWithIrodsSingleton, metaclass=ABCMeta):
             with open(data_object_location) as file:
                 return file.read()
 
-    def start_irobot_server(self, configuration_location: str, extra_links: Dict[str, str]=None) -> DockerisedService:
-        """
-        Starts an iRobot server against the iRODs singleton.
-        :param configuration_location: location of iRobot configuration to use
-        :param extra_links: containers that the iRobot container links to
-        :return: Dockerised iRobot server
-        """
-        extra_links = extra_links if extra_links is not None else {}
-        self._build_irobot()
-        self._irobot_controller = IrobotServiceController()
-        irobot_server = self._irobot_controller.start_service(dict(
-            volumes={self.irods_environment_json_location: dict(bind="/root/.irods/irods_environment.json", mode="ro"),
-                     configuration_location: dict(bind="/root/irobot.conf", mode="ro")},
-            environment={"IRODS_PASSWORD": self.irods.root_user.password},
-            links=dict(**{self.irods.container_id: self.irods.container_id}, **extra_links)))
-        return irobot_server
-
-    def create_irobot_configuration(self, basic_authentication_url: str) -> str:
+    def create_configuration(self, basic_authentication_url: str) -> str:
         """
         Creates configuration to use iRobot.
         :param basic_authentication_url: URL that provides basic authentication service
@@ -246,3 +251,50 @@ class TestWithIrobot(TestWithIrodsSingleton, metaclass=ABCMeta):
         with open(configuration_location, "w") as file:
             configuration.write(file)
         return configuration_location
+
+    def start_server(self, configuration_location: str, extra_links: Dict[str, str]=None) -> DockerisedService:
+        """
+        Starts an iRobot server against the iRODs singleton.
+        :param configuration_location: location of iRobot configuration to use
+        :param extra_links: containers that the iRobot container links to
+        :return: Dockerised iRobot server
+        """
+        extra_links = extra_links if extra_links is not None else {}
+        self._build_irobot()
+        self._irobot_controller = IrobotServiceController()
+        irobot_server = self._irobot_controller.start_service(dict(
+            volumes={self.irods.configuration_location: dict(bind="/root/.irods/irods_environment.json", mode="ro"),
+                     configuration_location: dict(bind="/root/irobot.conf", mode="ro")},
+            environment={"IRODS_PASSWORD": self.irods.service.root_user.password},
+            links=dict(**{self.irods.service.container_id: self.irods.service.container_id}, **extra_links)))
+        return irobot_server
+
+
+class TestWithIrodsSingleton(unittest.TestCase, metaclass=ABCMeta):
+    """
+    Tests that share an iRODS instance.
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls._standalone_irods = StandaloneIrods()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._standalone_irods.tear_down()
+
+    @property
+    def irods(self) -> StandaloneIrods:
+        return self._standalone_irods
+
+
+class TestWithIrobot(TestWithIrodsSingleton, metaclass=ABCMeta):
+    """
+    Tests that uses iRobot.
+    """
+    def setUp(self):
+        self.authentication_server = StandaloneAuthenticationServer()
+        self.irobot = StandaloneIrobot(self.authentication_server, self.irods)
+
+    def tearDown(self):
+        self.irobot.tear_down()
+        self.authentication_server.tear_down()
--- irobot/tests/system/test_irobot.py
+++ irobot/tests/system/test_irobot.py
@@ -1,3 +1,4 @@
+import logging
 import unittest
 from subprocess import CalledProcessError
 
@@ -10,27 +11,31 @@ class TestDataObjectEndpoint(TestWithIrobot):
     """
     Tests for iRobot's data object endpoints.
     """
-    def test_get_when_unauthorised(self):
-        basic_authentication_url = f"{self.basic_authentication_server.name}:" \
-                                   f"{list(self.basic_authentication_server.ports.keys())[0]}/" \
-                                   f"{BASIC_AUTHENTICATION_FAILED_STATUS_CODE}"
-        configuration_location = self.create_irobot_configuration(basic_authentication_url)
-        extra_links = {self.basic_authentication_server.name: self.basic_authentication_server.container_id}
+    def setUp(self):
+        super().setUp()
+        logging.root.setLevel(logging.DEBUG)
 
-        with self.start_irobot_server(configuration_location, extra_links) as irobot_service:
+    def test_get_when_unauthorised(self):
+        basic_authentication_url = f"{self.authentication_server.service.name}:" \
+                                   f"{list(self.authentication_server.service.ports.keys())[0]}/" \
+                                   f"{BASIC_AUTHENTICATION_FAILED_STATUS_CODE}"
+        configuration_location = self.irobot.create_configuration(basic_authentication_url)
+        extra_links = {self.authentication_server.service.name: self.authentication_server.service.container_id}
+
+        with self.irobot.start_server(configuration_location, extra_links) as irobot_service:
             irobot_url = f"http://{irobot_service.host}:{irobot_service.port}"
             try:
-                self.request_data("/some/data", irobot_url=irobot_url)
+                self.irobot.request_data("/some/data", irobot_url=irobot_url)
             except CalledProcessError as e:
                 # XXX: This madness is exploiting https://github.com/wtsi-hgi/irobot-client/issues/9
                 self.assertEqual(401 % 256, e.returncode)
 
     def test_get_when_not_in_irods(self):
-        self.assertIsNone(self.request_data("/not/real"))
+        self.assertIsNone(self.irobot.request_data("/not/real"))
 
     def test_get_when_in_irods(self):
-        data_object_location = self.upload_to_irods(EXAMPLE_DATA)
-        self.assertEqual(EXAMPLE_DATA, self.request_data(data_object_location))
+        data_object_location = self.irods.upload_file(EXAMPLE_DATA)
+        self.assertEqual(EXAMPLE_DATA, self.irobot.request_data(data_object_location))
 
 
 if __name__ == "__main__":
